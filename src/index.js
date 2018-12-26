db396e07d06c70ad2732551cf71d85a30cf28ab4
Osama Najjar <osama.najjar@mendix.com>
2018-12-26 23:25:08 +0100

    minimal mx widget code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import widgetBase from 'MxWidgetBase';
+import declare from 'dojoBaseDeclare';
+import * as widgetConf from '../conf/widget.config.json';
 
-export default class HyperWidget extends widgetBase {}
+export default declare(`${widgetConf.name}.${widgetConf.name}`, [ widgetBase ], {
+	constructor() {},
+	postCreate() {
+		console.debug(`${this.id} has been successfully initialized!`);
+	}
+});
