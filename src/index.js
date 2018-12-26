bf7b70b87cee54f5cf6286117259863e47b8bc06
Osama Najjar <osama.najjar@mendix.com>
2018-12-27 00:21:56 +0100

    created a hyperapp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import widgetBase from 'MxWidgetBase';
 import declare from 'dojoBaseDeclare';
 import * as widgetConf from '../conf/widget.config.json';
-
+import app from './app';
 export default declare(`${widgetConf.name}.${widgetConf.name}`, [ widgetBase ], {
 	constructor() {},
 	postCreate() {
 		console.debug(`${this.id} has been successfully initialized!`);
+		app(this.domNode);
 	}
 });
