c99f7e3602f586e61ccfd12ed1d4d00e60afe664
Osama Najjar <osama.najjar@mendix.com>
2018-12-27 13:37:23 +0100

    included styles

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const actions = {
 
 const view = (state, actions) => (
 	<div>
-		<h1>{state.count}</h1>
+		<h1 className="text">{state.count}</h1>
 		<button onclick={() => actions.down(1)}>-</button>
 		<button onclick={() => actions.up(1)}>+</button>
 	</div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import widgetBase from 'MxWidgetBase';
 import declare from 'dojoBaseDeclare';
 import * as widgetConf from '../conf/widget.config.json';
+import './style/style.scss';
+
 import app from './app';
 export default declare(`${widgetConf.name}.${widgetConf.name}`, [ widgetBase ], {
 	constructor() {},
